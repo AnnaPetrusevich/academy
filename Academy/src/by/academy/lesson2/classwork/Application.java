dd5789953df3b7d2c026ce2dd106a6ed0b1bdbda
igor <gob1in@Mail.ru>
2022-11-22 20:46:55 +0300

    kihoi

--- Academy/src/by/academy/lesson2/classwork/Application.java
+++
@@ -1,8 +0,0 @@
-package by.academy.lesson2.classwork;
-
-public class Application {
-	public static void main(String [] args) {
-		
-	}
-
-}
--- Academy/src/by/academy/lesson2/classwork/Cat2.java
+++
@@ -1,9 +0,0 @@
-package by.academy.lesson2.classwork;
-
-public class Cat2 {
-		Cat cat = new Cat();
-	}
-public class Cat2 {
-	Cat
-}
-
